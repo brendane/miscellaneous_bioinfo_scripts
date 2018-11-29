4605e0c5997360206bbf607e3238bd7cd11e35f7
brendane <epste051@umn.edu>
2018-11-29 16:55:04 -0600

    Automatic logout commit on 2018-11-29-16:55

---
+++ gff2fasta_prot.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python2.7
+"""
+    Given a gff file and a fasta file, output a file with protein
+    translations of genes.
+
+    gff2fasta_prot.py [--prefix <prefix>] <gff> <fasta>
+"""
+
+import argparse
+import csv
+import sys
+
+from Bio import SeqIO, Seq
+
+parser = argparse.ArgumentParser(usage=__doc__)
+parser.add_argument('--prefix')
+parser.add_argument('gff')
+parser.add_argument('fasta')
+args = parser.parse_args()
+
+prefix = args.prefix
+
+idx = SeqIO.index(args.fasta, 'fasta')
+
+with open(args.gff, 'rb') as ih:
+    rdr = csv.reader(ih, delimiter='\t')
+    for row in rdr:
+        chrom = row[0]
+        start = int(row[3]) - 1
+        end = int(row[4])
+        strand = row[6]
+        lt = {x.split('=')[0]:x.split('=')[1]
+              for x in row[8].split(';')}['locus_tag']
+        seq = idx[chrom][start:end]
+        overhang = 3 - (len(seq) % 3)
+        if overhang != 3:
+            seq.seq += 'N' * overhang
+        if strand == '-':
+            seq = seq.reverse_complement()
+        sys.stdout.write('>' + lt + '\n')
+        sys.stdout.write(str(seq.seq.translate()) + '\n')
