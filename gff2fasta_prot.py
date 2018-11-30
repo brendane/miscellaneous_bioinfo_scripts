5bfaa09c3e6281827a0116e6b8b590dab32df7fb
brendane <epste051@umn.edu>
2018-11-30 16:09:56 -0600

    Automatic logout commit on 2018-11-30-16:09

--- gff2fasta_prot.py
+++ gff2fasta_prot.py
@@ -18,9 +18,13 @@ parser.add_argument('gff')
 parser.add_argument('fasta')
 args = parser.parse_args()
 
-prefix = args.prefix
+if args.prefix is not None:
+    prefix = args.prefix
+else:
+    prefix = ''
 
 idx = SeqIO.index(args.fasta, 'fasta')
+lengths = {i:len(idx[i]) for i in idx}
 
 with open(args.gff, 'rb') as ih:
     rdr = csv.reader(ih, delimiter='\t')
@@ -31,11 +35,15 @@ with open(args.gff, 'rb') as ih:
         strand = row[6]
         lt = {x.split('=')[0]:x.split('=')[1]
               for x in row[8].split(';')}['locus_tag']
-        seq = idx[chrom][start:end]
+        if end > lengths[chrom]-1:
+            seq = idx[chrom][start:] + \
+                    idx[chrom][0:(end-lengths[chrom])]
+        else:
+                seq = idx[chrom][start:end]
         overhang = 3 - (len(seq) % 3)
         if overhang != 3:
             seq.seq += 'N' * overhang
         if strand == '-':
             seq = seq.reverse_complement()
-        sys.stdout.write('>' + lt + '\n')
+        sys.stdout.write('>' + prefix + lt + '\n')
         sys.stdout.write(str(seq.seq.translate()) + '\n')
