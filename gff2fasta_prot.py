5fee2069563882ad6849268cc649b45a8452fd26
brendane <epste051@umn.edu>
2018-12-01 20:08:25 -0600

    Automatic logout commit on 2018-12-01-20:08

--- gff2fasta_prot.py
+++ gff2fasta_prot.py
@@ -3,23 +3,40 @@
     Given a gff file and a fasta file, output a file with protein
     translations of genes.
 
-    gff2fasta_prot.py [--id <id field (locus_tag)>] [--prefix <prefix>] <gff> <fasta>
+    gff2fasta_prot.py [--no-dups] [--id <id field (locus_tag)>]
+        [--table <trans table (Bacterial)>]
+        [--prefix <prefix>] <gff> <fasta>
+
+    Uses Bacterial translation table, and checks if the sequences is a
+    true coding sequence. If it is, alternative start codons are translated
+    as "M". Ns are added to the end to make a sequence
+    with complete codons after reverse-complementing and before
+    translation.
+
+    If the id field is not present, the record is skipped. If the
+    no-dups flag is set, sequences with the same name and length
+    are skipped after they are first seen.
 """
 
 import argparse
 import csv
 import sys
 
-from Bio import SeqIO, Seq
+from Bio import SeqIO
+from Bio.Data.CodonTable import TranslationError
 
 parser = argparse.ArgumentParser(usage=__doc__)
 parser.add_argument('--prefix')
+parser.add_argument('--table', default='Bacterial')
 parser.add_argument('--id', default='locus_tag')
+parser.add_argument('--no-dups', default=False, action='store_true')
 parser.add_argument('gff')
 parser.add_argument('fasta')
 args = parser.parse_args()
 
+table = args.table
 id_field = args.id
+nd = args.no_dups
 if args.prefix is not None:
     prefix = args.prefix
 else:
@@ -27,26 +44,38 @@ else:
 
 seqs = {rec.id:rec for rec in SeqIO.parse(args.fasta, 'fasta')}
 lengths = {i:len(seqs[i]) for i in seqs}
+seen = {}
 
 with open(args.gff, 'rb') as ih:
     rdr = csv.reader(ih, delimiter='\t')
     for row in rdr:
+        if row[0].startswith('#'):
+            continue
         chrom = row[0]
         start = int(row[3]) - 1
         end = int(row[4])
         strand = row[6]
-        lt = {x.split('=')[0]:x.split('=')[1]
-              for x in row[8].split(';')}[id_field]
+        try:
+            lt = {x.split('=')[0]:x.split('=')[1]
+                  for x in row[8].split(';')}[id_field]
+        except KeyError:
+            continue
         if end > lengths[chrom]-1:
             #seq = seqs[chrom][start:] + \
             #        seqs[chrom][0:(end-lengths[chrom])]
             seq = seqs[chrom][start:]
         else:
                 seq = seqs[chrom][start:end]
+        if lt in seen and seen[lt] == len(seq):
+            continue
+        seen[lt] = len(seq)
+        if strand == '-':
+            seq = seq.reverse_complement()
         overhang = 3 - (len(seq) % 3)
         if overhang != 3:
             seq.seq += 'N' * overhang
-        if strand == '-':
-            seq = seq.reverse_complement()
         sys.stdout.write('>' + prefix + lt + '\n')
-        sys.stdout.write(str(seq.seq.translate()) + '\n')
+        try:
+            sys.stdout.write(str(seq.seq.translate(table=table, cds=True)) + '\n')
+        except TranslationError:
+            sys.stdout.write(str(seq.seq.translate(table=table)) + '\n')
---
+++ gff_filter_tags.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+"""
+    Filter a GFF file based on tags.
+
+    gff_filter_tags.py [--only-present] <gff file> <tag1=value1,value2 ...> <tag2=...>
+"""
+
+import argparse
+import csv
+import sys
+
+parser = argparse.ArgumentParser(usage=__doc__)
+parser.add_argument('--only-present', default=False, action='store_true')
+parser.add_argument('gff')
+parser.add_argument('filters', nargs='+')
+args = parser.parse_args()
+
+op = args.only_present
+filters = {}
+if args.filters[0] != "":
+    filters = {x.split('=')[0]:[y for y in x.split('=')[1].split(',')]
+               for x in args.filters}
+
+
+with open(args.gff, 'rb') as handle:
+    rdr = csv.reader(handle, delimiter='\t')
+    for row in rdr:
+        if row[0].startswith('#'):
+            sys.stdout.write('\t'.join(row) + '\n')
+            continue
+        tags = {x.split('=')[0]:x.split('=')[1] for x in row[8].split(';')}
+        ok = True
+        for f in filters:
+            if f in tags:
+                if tags[f] not in filters[f]:
+                    ok = False
+                    break
+            elif op:
+                ok = False
+                break
+        if ok:
+            sys.stdout.write('\t'.join(row) + '\n')
