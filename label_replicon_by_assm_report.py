dc4a0097936ad61c12bc35aa87157e6962308c99
brendane <epste051@umn.edu>
2019-02-18 15:17:50 -0600

    Automatic logout commit on 2019-02-18-15:17

---
+++ label_replicon_by_assm_report.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+    Label replicons in a fasta file by the Assigned-Molecule-Location
+    column.
+
+    label_replicon_by_assm_report.py <fasta> <report>
+"""
+
+import sys
+
+from Bio import SeqIO
+
+replicons = {}
+with open(sys.argv[2], 'r') as ih:
+    for line in ih:
+        if line.startswith('#'):
+            continue
+        fields = line.strip().split('\t')
+        n = fields[6]
+        r = fields[3]
+        replicons[n] = r
+
+for rec in SeqIO.parse(sys.argv[1], 'fasta'):
+    r = replicons[rec.id]
+    sys.stdout.write(r + ':' + rec.id + '\n')
+    sys.stdout.write(str(rec.seq) + '\n')
---
+++ mummer_replicon_assignments.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+    Produce a table of replicon assignments from show-coords output
+    with the -q -T and -l options. Assumes that the reference contigs
+    are named like so: <replicon>:<whatever>.
+
+    It also assumes that the alignments were filtered with delta-filter
+    -q -o 0 (these are both important).
+
+    mummer_replicon_assignments.py <input file>
+"""
+
+#==============================================================================#
+
+import collections
+import csv
+import itertools
+import re
+import sys
+
+#==============================================================================#
+
+replicons = set()
+lengths = {}
+cov = {}
+with open(sys.argv[1], 'rb') as handle:
+    for i in range(4): handle.readline()
+    rdr = csv.reader(handle, delimiter='\t')
+    for qry, rows in itertools.groupby(rdr, lambda x: x[10]):
+        qry_len = None
+        qry_cov = collections.defaultdict(int)
+        for row in rows:
+            ref = re.sub(':.+', '', row[9])
+            replicons.add(ref)
+            qry_cov[ref] += int(row[5])
+            ql = float(row[8])
+            if qry_len is None:
+                qry_len = ql
+            elif ql != qry_len:
+                raise Exception('lengths do not match for %s' % qry)
+        lengths[qry] = qry_len
+        cov[qry] = qry_cov
+replicons = sorted(replicons)
+
+
+sys.stdout.write('contig\t' + '\t'.join(replicons) + '\tother_unassigned\n')
+for qry in cov:
+    qry_cov = cov[qry]
+    qry_len = lengths[qry]
+    sys.stdout.write(qry)
+    c = 0.
+    for r in replicons:
+        cv = qry_cov[r] / qry_len
+        c += cv
+        sys.stdout.write('\t' + str(cv))
+    ou = 1 - c
+    sys.stdout.write('\t' + str(ou) + '\n')
