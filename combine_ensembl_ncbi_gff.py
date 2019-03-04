25af2faa5e4a568586cb5d640eecba18e289f8f4
brendane <epste051@umn.edu>
2019-03-04 09:52:16 -0600

    Automatic logout commit on 2019-03-04-09:52

---
+++ combine_ensembl_ncbi_gff.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""
+    Combine ENSEMBL annotation file (tab-delimited) with NCBI gff file.
+    Made specifically for dolphin project.
+"""
+
+import argparse
+import collections
+import csv
+import re
+import sys
+
+parser = argparse.ArgumentParser(usage=__doc__)
+parser.add_argument('--output')
+parser.add_argument('ensembl')
+parser.add_argument('gff')
+args = parser.parse_args()
+
+gff_info = {}
+with open(args.gff, 'r') as ih:
+    rdr = csv.reader(ih, delimiter='\t')
+    for row in rdr:
+        if row[0].startswith('#'):
+            continue
+        if row[2] == 'gene':
+            chrom = row[0]
+            start = int(row[3])
+            end = int(row[4])
+            annot = row[8]
+            gid = re.sub('GeneID:', '',
+                         {x.split('=')[0]:x.split('=')[1] for x in annot.split(';')}['Dbxref'])
+            gff_info[gid] = (chrom, start, end)
+        else:
+            continue
+
+go_annot = {}
+gene_go = collections.defaultdict(set)
+errors = set()
+with open(args.ensembl, 'r') as ih:
+    rdr = csv.DictReader(ih, delimiter='\t')
+    for row in rdr:
+        egid = row['Gene stable ID']
+        estart = int(row['Gene start (bp)'])
+        eend = int(row['Gene end (bp)'])
+        desc = row['Gene description']
+        if 'NCBI' not in desc:
+            errors.add(('%s in ensembl but not ncbi' % egid))
+            continue
+        gid = re.sub('\]', '',
+                     re.sub('.+ \[Source:NCBI gene;Acc:', '', desc))
+        echrom = row['Chromosome/scaffold name']
+        go = row['GO term accession']
+        go_desc = row['GO term name']
+        if gid not in gff_info:
+            errors.add(('%s in ensembl but not ncbi' % gid))
+        if gff_info[gid][1] != estart:
+            errors.add(('%s positions do not match (%i, %i) in ncbi and (%i, %i) in ens.'
+                        %(gid, gff_info[gid][1], gff_info[gid][2], estart, eend)))
+        gene_go[gid].add(go)
+        go_annot[go] = go_desc
+
+for error in errors:
+    sys.stdout.write(error + '\n')
