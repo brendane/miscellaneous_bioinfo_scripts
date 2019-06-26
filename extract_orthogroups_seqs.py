9cb79465474545593279e19ec5ecd2903208d572
brendane <epste051@umn.edu>
2019-06-26 14:07:13 -0500

    Automatic logout commit on 2019-06-26-14:07

--- extract_orthogroups_seqs.py
+++ extract_orthogroups_seqs.py
@@ -3,7 +3,8 @@
     Given the Orthogroups.tsv file from OrthoFinder 2.3, extract
     unaligned files of sequences.
 
-    extract_orthogroups_seqs.py [--gene-list] <Orthogroups.tsv> <fasta> <output directory>
+    extract_orthogroups_seqs.py [--ignore-missing] [--gene-list]
+        <Orthogroups.tsv> <fasta> <output directory>
 """
  
 import argparse
@@ -12,6 +13,7 @@ import csv
 from Bio import SeqIO
 
 parser = argparse.ArgumentParser(usage=__doc__)
+parser.add_argument('--ignore-missing', default=False, action='store_true')
 parser.add_argument('--gene-lists')
 parser.add_argument('orthogroups')
 parser.add_argument('fasta')
@@ -40,4 +42,7 @@ with open(args.orthogroups) as ih:
                     try:
                         SeqIO.write(seqs[g], oh, 'fasta')
                     except KeyError:
-                        raise Exception('%s not found in %s' % (g, args.fasta))
+                        if args.ignore_missing:
+                            pass
+                        else:
+                            raise Exception('%s not found in %s' % (g, args.fasta))
