be3540dea9ecbe49f9bf386022ae7789f72d3b7f
brendane <epste051@umn.edu>
2019-10-28 16:34:59 -0500

    Automatic logout commit on 2019-10-28-16:34

---
+++ consensus_from_maf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+    Get a consensus sequence from an MAF file.
+
+    consensus_from_maf.py <input file>
+"""
+
+import collections
+import sys
+
+with open(sys.argv[1], 'rt') as ih:
+    annots = {}
+    counters = []
+    for line in ih:
+        if line.startswith('a'):
+            if len(counters) > 0:
+                for c in counters:
+                    sys.stdout.write(c.most_common()[0][0])
+                sys.stdout.write('\n')
+            annots = {x.split('=')[0]:x.split('=')[1] for x in line.strip().split()[1:]}
+            counters = []
+            sys.stdout.write('>' + annots['label'] + '\n')
+        elif line.startswith('s'):
+            fields = line.strip().split()
+            s = fields[6]
+            if len(counters) == 0:
+                counters = [collections.Counter() for x in s]
+            for i, b in enumerate(s):
+                if b != '-':
+                    counters[i].update(b)
+        else:
+            continue
+    for c in counters:
+        sys.stdout.write(c.most_common()[0][0])
+    sys.stdout.write('\n')
