e402261bc5b5a202c47629984bfa1bd6876e4e80
brendane <epste051@umn.edu>
2019-10-31 17:07:18 -0500

    Automatic logout commit on 2019-10-31-17:07

--- jaccard_index.py
+++ jaccard_index.py
@@ -3,7 +3,7 @@
     Calculate the Jaccard index given a tsv file with a column containing
     comma-separated lists of items.
 
-    jaccard_index.py <input file> <column>
+    jaccard_index.py <input file> <column> [<gene list> <gene column>]
 """
 
 import argparse
@@ -15,13 +15,22 @@ parser = argparse.ArgumentParser(usage=__doc__)
 parser.add_argument('--min-strains', default=0, type=int)
 parser.add_argument('input')
 parser.add_argument('column')
+parser.add_argument('genes', nargs='*', default=[])
 args = parser.parse_args()
+
+keep = set()
+if len(args.genes):
+    with open(args.genes[0], 'rt') as ih:
+        for line in ih:
+            keep.add(line.strip())
 
 shared = collections.defaultdict(int)
 total = collections.defaultdict(int)
 with open(args.input, 'rt') as ih:
     rdr = csv.DictReader(filter(lambda x: not x.startswith('#'), ih), delimiter='\t')
     for i, row in enumerate(rdr):
+        if len(args.genes) > 0 and row[args.genes[1]] not in keep:
+            continue
         groups = row[args.column].split(',')
         if len(groups) < args.min_strains:
             continue
